32d365331281dbbf03d896b9db545c5857185713
Chris <cmecinski@gmail.com>
2023-05-10 20:44:04 -0500

    added food + scoreboard

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
-#Create starting Screen
+# Create starting Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-#Turns animation off to allow it to be manually updated
+# Turns animation off to allow it to be manually updated
 screen.tracer(0)
 
-#Create Starting 3 tile Snake
+# Create Starting 3 tile Snake, Food, and Scoreboard
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -25,6 +29,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
